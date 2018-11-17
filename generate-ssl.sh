4e6b05177b68bd6282b0ab681d747186a8d55f71
Gibran Malheiros <gibatronic@gmail.com>
2018-11-17 12:32:54 +0100

    wrapped interpolations in quotes
    to avoid bad surprises, like file names with spaces

--- generate-ssl.sh
+++ generate-ssl.sh
@@ -43,23 +43,23 @@ sed -i '' '/^CN/ d' certificate-authority-options.conf
 echo "CN = ${name}" >> certificate-authority-options.conf
 
 # Generate Certificate Authority
-openssl genrsa -des3 -out tmp/${name}CA.key 2048
-openssl req -x509 -config certificate-authority-options.conf -new -nodes -key tmp/${name}CA.key -sha256 -days 1825 -out your-certs/${name}CA.pem
+openssl genrsa -des3 -out "tmp/${name}CA.key" 2048
+openssl req -x509 -config certificate-authority-options.conf -new -nodes -key "tmp/${name}CA.key" -sha256 -days 1825 -out "your-certs/${name}CA.pem"
 
 if command_exists security ; then
     # Delete trusted certs by their common name via https://unix.stackexchange.com/a/227014
     security find-certificate -c "${name}" -a -Z | sudo awk '/SHA-1/{system("security delete-certificate -Z "$NF)}'
 
     # Trust the Root Certificate cert
-    security add-trusted-cert -d -r trustRoot -k /Library/Keychains/System.keychain your-certs/${name}CA.pem
+    security add-trusted-cert -d -r trustRoot -k /Library/Keychains/System.keychain "your-certs/${name}CA.pem"
 fi
 
 # Generate CA-signed Certificate
-openssl genrsa -out your-certs/${name}.key 2048
-openssl req -new -config certificate-authority-options.conf -key your-certs/${name}.key -out tmp/${name}.csr
+openssl genrsa -out "your-certs/${name}.key" 2048
+openssl req -new -config certificate-authority-options.conf -key "your-certs/${name}.key" -out "tmp/${name}.csr"
 
 # Generate SSL Certificate
-openssl x509 -req -in tmp/${name}.csr -CA your-certs/${name}CA.pem -CAkey tmp/${name}CA.key -CAcreateserial -out your-certs/${name}.crt -days 1825 -sha256 -extfile options.conf
+openssl x509 -req -in "tmp/${name}.csr" -CA "your-certs/${name}CA.pem" -CAkey "tmp/${name}CA.key" -CAcreateserial -out "your-certs/${name}.crt" -days 1825 -sha256 -extfile options.conf
 
 # Cleanup a stray file
 rm your-certs/*.srl
